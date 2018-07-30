51c44544d647e4bf650a6306320e253ba12fcb72
Jessica <jessica0greene@gmail.com>
2018-07-30 20:03:28 +0200

    initial commit of working files

---
+++ webserver.go
@@ -0,0 +1,94 @@
+/*
+A simple web server, using only the standard library package of go:
+
+* Only accepts POST requests
+(405 Method Not Allowed : A request method is not supported for the requested resource; for example, a GET request on a form that requires data to be presented via POST, or a PUT request on a read-only resource.)
+* For any request, checks the body for a json encoded object and returns a descriptive HTTP error code if the body does not contain a valid json object.
+	We expect this object to look like this: {"favoriteTree": "baobab"}
+* Only accepts requests on the index URL: "/", and returns the proper HTTP error code if a different URL is requested.
+* Runs locally on port 8000
+* For a successful request, returns a properly encoded HTML document with the following content:
+	If "favoriteTree" was specified:
+		It's nice to know that your favorite tree is a <value of "favoriteTree" in the POST body>
+	if not specified:
+		Please tell me your favorite tree
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+type responseData struct {
+	FavoriteTree string
+}
+
+func responseHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" { // only allow requests from root
+		http.NotFound(w, r) // return 404 (not found)
+		return
+	}
+
+	if r.Method != "POST" { //return 405 if not POST (Method not allowed)
+		http.Error(w, "Please only use Post requests", 405)
+		return
+	}
+
+	var p responseData
+	err := json.NewDecoder(r.Body).Decode(&p)
+
+	switch {
+	case err == io.EOF:
+		http.Error(w, "Body of request cannot be empty, expecting Json data.", 412) //return 412 if body empty (Precondition Failed)
+		return
+	case err != nil:
+		http.Error(w, err.Error(), 400) // return 400 (Bad Request) for other errors and error message
+		return
+	}
+
+	if len(p.FavoriteTree) > 0 {
+		t, err := template.ParseFiles("./templates/favoriteTree.html")
+		t.Execute(w, p)
+		if err != nil {
+			http.Error(w, err.Error(), 400) // return 400 (Bad Request) for other errors and error message
+			return
+		}
+	} else {
+		t, err := template.ParseFiles("./templates/noTree.html")
+		t.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), 400) // return 400 (Bad Request) for other errors and error message
+			return
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", responseHandler)    // set router
+	err := http.ListenAndServe(":8000", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+/* Curl commands to test code:
+(For windows need to escape " and use only double quotes.)
+
+*Post request with correct body
+curl -s -S -XPOST -d"{\"favoriteTree\":\"Beech\"}" http://localhost:8000
+
+*Post request without body
+curl -s -S -XPOST http://localhost:8000
+
+*Post request with wrong body content
+curl -s -S -XPOST -d"{\"name\":\"Beech\"}" http://localhost:8000
+
+*Get request
+curl http://localhost:8000
+
+*/
---
+++ webserver_test.go
@@ -0,0 +1,141 @@
+/*
+TEST CASES:
+* Non-POST requests return 405 (Method Not Allowed)
+* No JSON found in body returns a descriptive HTTP error code
+* Check objest return matches this {"favoriteTree": "baobab"}
+* Non-index URL: "/", returnsproper HTTP error code
+* Check that for a successful request, returns a properly encoded HTML document with the following content:
+	If "favoriteTree" was specified:
+		It's nice to know that your favorite tree is a <value of "favoriteTree" in the POST body>
+	if not specified:
+		Please tell me your favorite tree
+*/
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type testBody struct {
+	FavoriteTree string
+}
+
+type testFalseBody struct {
+	Name string
+}
+
+func Test_responseHandler_1(t *testing.T) {
+	// Test with POST method and from root with no body
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(responseHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusPreconditionFailed {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusPreconditionFailed)
+	}
+}
+
+func Test_responseHandler_2(t *testing.T) {
+	// Test with POST method but not root URL
+	req, err := http.NewRequest("POST", "/smth", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(responseHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNotFound)
+	}
+}
+
+func Test_responseHandler_3(t *testing.T) {
+	// Test with POST method and correct body
+	tb := testBody{FavoriteTree: "Beech"}
+	b, err := json.Marshal(tb)
+	r := bytes.NewReader(b)
+	req, err := http.NewRequest("POST", "/", r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(responseHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func Test_responseHandler_4(t *testing.T) {
+	// Check response body with POST method and correct body
+	tb := testBody{FavoriteTree: "Oak"}
+	b, err := json.Marshal(tb)
+	r := bytes.NewReader(b)
+	req, err := http.NewRequest("POST", "/", r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(responseHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// Check the response body is what we expect by length.
+	expected := 1589
+	if len(rr.Body.String()) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			len(rr.Body.String()), expected)
+	}
+}
+
+func Test_responseHandler_5(t *testing.T) {
+	// Check response body with POST method and incorrect body
+	tb := testFalseBody{Name: "Jessica"}
+	b, err := json.Marshal(tb)
+	r := bytes.NewReader(b)
+	req, err := http.NewRequest("POST", "/", r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(responseHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// Check the response body is what we expect by length.
+	expected := 1714
+	if len(rr.Body.String()) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			len(rr.Body.String()), expected)
+	}
+}
